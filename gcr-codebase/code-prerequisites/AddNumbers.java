5a084db614b91653fd2e302d04cb47804f58764e
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-16 17:22:39 +0530

    [Mohit Dixit] add two number

---
+++ gcr-codebase/code-prerequisites/AddNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AddNumbers.java{
+
+public static void main(String[],args){
+ int num1;
+ int num2;
+ Scanner sc = new Scanner(System.in);
+ 
+ System.out.println("Enter first number: ");
+ a = sc.nextInt();
+ System.out.println("Enter second number: ");
+ b = sc.nextInt();
+
+	System.out.println("Sum =" + (a+b));
+}
+}
