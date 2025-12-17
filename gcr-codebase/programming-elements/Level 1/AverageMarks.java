999465bc8a867c4c306ccd22b247f0a44a20d381
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-18 02:10:53 +0530

    [Mohit Dixit] - Average Marks added successfully

---
+++ gcr-codebase/programming-elements/Level 1/AverageMarks.java
@@ -0,0 +1,12 @@
+public class AverageMarks{
+public static void main(String[] args){
+
+	int mathMarks = 94;
+	int physicsMarks = 95;
+	int chemistryMarks = 96;
+	
+	double average = (mathMarks+physicsMarks+chemistryMarks)/3.0;
+	
+	System.out.println("Sam's average mark in PCM is " + average);
+}
+}
