ff0b8583edfffa1468bf1596843be050bd2daf67
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-18 02:05:31 +0530

    [Mohit Dixit] - Age Finder added

---
+++ gcr-codebase/programming-elements/Level 1/AgeFinder.java
@@ -0,0 +1,10 @@
+public class AgeFinder{
+public static void main(String[] args){
+
+	int age ;
+	int birthYear = 2000;
+	int currentYear = 2024;
+	
+	System.out.println("Harry's age in 2024 is " + (birthYear-currentYear));
+}
+}
