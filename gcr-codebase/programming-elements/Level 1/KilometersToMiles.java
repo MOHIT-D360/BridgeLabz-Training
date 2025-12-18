848b6bc6f7c08611e8b62ccaf61796ab9877866c
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-18 10:07:31 +0530

    [Mohit Dixit] - Kilometers to miles converter added successfully

---
+++ gcr-codebase/programming-elements/Level 1/KilometersToMiles.java
@@ -0,0 +1,12 @@
+public class KilometersToMiles{
+public static void main(String[] args){
+
+	double distance = 10.8;
+	double distanceInMiles = distance * 1.6;
+	
+	
+	
+	
+	System.out.println("The distance" + distance + "km in miles is" + distanceInMiles);
+}
+}
