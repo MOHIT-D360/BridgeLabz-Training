2a59ce20f7824f139c7b3e78a14d6d0c214372c3
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-19 12:49:03 +0530

    [Mohit Dixit] - Greatest factor finder using while loop added successfully

---
+++ gcr-codebase/control-flows/Level 2/GreatestFactorWhileLoop.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class GreatestFactorWhileLoop {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number");
+        int number = sc.nextInt();
+
+        int greatestFactor = -1;
+        int i=1;
+        while(i<=(number/2)){
+             if(number%i ==0){
+              if(greatestFactor<i){
+                greatestFactor = i;
+              }
+
+            }
+            i++;
+        }
+
+      
+            System.out.println("Greatest factor of number "+ number+ " is "+greatestFactor);
+        }
+}
