9d3bbfd640a5ba6adda7709e054ec54e7392fc75
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-22 01:25:43 +0530

    [Mohit Dixit] : Leap year finder added

---
+++ gcr-codebase/methods/Level 2/LeapYear.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static boolean isLeapYear(int year){      
+        if((year %4 ==0 && year %100 !=0) || (year %400 ==0)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt(); 
+
+        if(year>=1532){
+         boolean leapYear =    isLeapYear(year);
+            if(leapYear){
+                System.out.println(year + " is a leap year.");
+            }
+            else{
+                System.out.println(year + " is not a leap year.");
+            }
+        }
+
+    }
+}
