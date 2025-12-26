3192033085a6e241cefc9252fb76f4e22109567d
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-26 20:35:40 +0530

    [Mohit] Hand book added

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/HandBook.java
@@ -0,0 +1,28 @@
+package com.classandobject.level1;
+
+public class HandBook {
+		String  bookTitle ;
+		String bookAuthor;
+		double price;
+		
+		HandBook(String bookTitle,String bookAuthor, double price){
+			this.bookTitle = bookTitle;
+			this.bookAuthor = bookAuthor;
+			this.price = price;
+			
+			
+		}
+		
+		public void displayBookDetails() {
+			System.out.println("Title of the book: "+bookTitle);
+			System.out.println("Author of the book: "+bookAuthor);
+			System.out.println("Price of the book: "+price);
+		}
+
+	public static void main(String[] args) {
+		HandBook book1= new HandBook("Discovery of India", "Jawaharlal Nehru", 550);
+		book1.displayBookDetails();
+
+	}
+
+}
