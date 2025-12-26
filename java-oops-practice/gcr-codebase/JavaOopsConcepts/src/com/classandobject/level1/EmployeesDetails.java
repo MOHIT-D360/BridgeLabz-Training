3094ae3a852a85e9334cb23cc9651f80e8611c9e
MOHIT-D360 <mohitnsp786@gmail.com>
2025-12-26 14:42:31 +0530

    Added branch and Employees details

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/classandobject/level1/EmployeesDetails.java
@@ -0,0 +1,10 @@
+package com.classandobject.level1;
+
+public class EmployeesDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Employees");
+	}
+
+}
