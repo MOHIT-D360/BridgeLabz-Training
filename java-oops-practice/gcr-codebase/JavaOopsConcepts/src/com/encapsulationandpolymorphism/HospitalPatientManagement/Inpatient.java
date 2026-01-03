1e59d703165ffc0a994e07813bceabf5e3501add
MOHIT-D360 <mohitnsp786@gmail.com>
2026-01-03 13:20:32 +0530

    [Mohit] added hospital patient management system using encapsulation

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/HospitalPatientManagement/Inpatient.java
@@ -0,0 +1,28 @@
+package com.encapsulationandpolymorphism.HospitalPatientManagement;
+
+class Inpatient extends Patient implements MedicalRecord {
+
+    private int daysAdmitted;
+    private double dailyCharge;
+
+    public Inpatient(int id, String name, int age, int days, double charge) {
+        super(id, name, age);
+        this.daysAdmitted = days;
+        this.dailyCharge = charge;
+    }
+
+    @Override
+    double calculateBill() {
+        return daysAdmitted * dailyCharge;
+    }
+
+    @Override
+    public void addRecord(String record) {
+        System.out.println("In-Patient Record Added: " + record);
+    }
+
+    @Override
+    public void viewRecords() {
+        System.out.println("Viewing In-Patient Medical Records");
+    }
+}
---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/HospitalPatientManagement/OutPatient.java
@@ -0,0 +1,27 @@
+package com.encapsulationandpolymorphism.HospitalPatientManagement;
+
+class OutPatient extends Patient implements MedicalRecord {
+
+    private double consultationFee;
+
+    public OutPatient(int id, String name, int age, double fee) {
+        super(id, name, age);
+        this.consultationFee = fee;
+    }
+
+    @Override
+    double calculateBill() {
+        return consultationFee;
+    }
+
+    @Override
+    public void addRecord(String record) {
+        System.out.println("Out-Patient Record Added: " + record);
+    }
+
+    @Override
+    public void viewRecords() {
+        System.out.println("Viewing Out-Patient Medical Records");
+    }
+}
+
