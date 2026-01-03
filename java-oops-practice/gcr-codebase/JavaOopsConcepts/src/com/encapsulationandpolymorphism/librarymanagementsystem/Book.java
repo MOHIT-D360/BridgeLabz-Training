4bcab20192a124a899f83e718420a28e09093b95
MOHIT-D360 <mohitnsp786@gmail.com>
2026-01-03 13:23:39 +0530

    [Mohit] : added library management system using encapsulation

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/librarymanagementsystem/Book.java
@@ -0,0 +1,26 @@
+package com.encapsulationandpolymorphism.librarymanagementsystem;
+
+public class Book extends LibraryItem implements Reservable{
+	private boolean available  = true;
+	
+	public Book(int itemId,String title,String author) {
+		super(itemId,title,author);
+	}
+	String getLoanDuration() {
+		return "14 days";
+	}
+	
+	public void reserveItem() {
+		if(available) {
+			available  = false;
+			System.out.println("Book reserved successfully");
+		}
+		else {
+			System.out.println("Book is already reserved");
+		}
+	}
+	
+	public boolean checkAvailability() {
+		return available;
+	}
+}
