5c9ae210a55fde225fb188dacc12c71d36f72ebc
MOHIT-D360 <mohitnsp786@gmail.com>
2026-01-03 13:25:18 +0530

    [Mohit] : added ride hailing application of vehicles

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/encapsulationandpolymorphism/ridehailingapplicaiton/TestVehicle.java
@@ -0,0 +1,22 @@
+package com.encapsulationandpolymorphism.ridehailingapplicaiton;
+
+public class TestVehicle {
+	public static void main(String [] args) {
+	Vehicle[] varr = new Vehicle[3];
+	varr[0] = new Car(112,"Mohit",20,"Bhopal");
+	varr[1] = new Bike(113,"Subham",8,"Indore");
+	varr[2] = new Auto(114,"Vishaal",15,"Bhopal");
+	
+	for(Vehicle v : varr) {
+		v.getVehicleDetails();
+		System.out.println(v.calculateFare(15));
+		
+		if(v instanceof GPS) {
+			System.out.println("Current Location :"+((GPS) v).getCurrentLocation());
+		}
+		
+		System.out.println("_______________________________________");
+	}
+	
+	}
+}
