b0124b02cb515a05a29943c3b52282567b061351
MOHIT-D360 <mohitnsp786@gmail.com>
2026-01-09 09:20:17 +0530

    [Mohit] : String reverser added

---
+++ dsa-practice/gcr-codebase/JavaDsaProject/src/com/searchingalgorithm/stringbuilder/Reverse.java
@@ -0,0 +1,8 @@
+package com.searchingalgorithm.stringbuilder;
+
+public class Reverse {
+	public static void main(String[] agrs) {
+		StringBuilder sb = new StringBuilder("hello");
+		System.out.println(sb.reverse());
+	}
+}
