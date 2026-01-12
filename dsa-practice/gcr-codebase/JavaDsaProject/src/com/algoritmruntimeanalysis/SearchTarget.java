3d61232ee6875b61793ddb36231226a79c1feba8
MOHIT-D360 <mohitnsp786@gmail.com>
2026-01-12 23:22:50 +0530

    [Mohit] : compare linear search and binary search in nanoseconds

---
+++ dsa-practice/gcr-codebase/JavaDsaProject/src/com/algoritmruntimeanalysis/SearchTarget.java
@@ -0,0 +1,50 @@
+package com.algoritmruntimeanalysis;
+
+public class SearchTarget {
+    public static void linearSearch(int[] brr,int target){
+        for(int i=0;i<brr.length;i++){
+            if(brr[i] == target){
+                System.out.println("Found");
+                break;
+            }
+        }
+        System.out.println("Not Found");
+    }
+    public static void binarySearch(int [] brr,int target){
+        int low = 0;
+        int high = brr.length -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(brr[mid]== target){
+                System.out.println("found  : "+ mid);
+                return ;
+            }
+            else if(target<brr[mid]){
+                high = mid-1;
+            }
+            else low= mid+1;
+        }
+        return;
+    }
+    public static void main(String[] args) {
+        int n = 10000;
+        int [] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]= i;
+        }
+        int target =-1;
+
+        long startLinearTime = System.nanoTime();
+        linearSearch(arr,target);
+        long endLinearTime = System.nanoTime();
+
+        System.out.println("Linear search time is :"+ (endLinearTime-startLinearTime)+ "ms");
+
+
+         long startBinaryTime = System.nanoTime();
+        binarySearch(arr,target);
+        long endBinaryTime = System.nanoTime();
+
+        System.out.println("Binary search time is :"+ (endBinaryTime-startBinaryTime)+ "ms");
+    }
+}
