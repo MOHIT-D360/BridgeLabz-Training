db5b2055b05d6e331d5a697b87bb032848e70151
MOHIT-D360 <mohitnsp786@gmail.com>
2026-02-01 21:44:32 +0530

    [Mohit] : added program to convert csv to java object

---
+++ java-iostreams-practice/gcr-codebase/javaiostreampractice/src/com/csvhandling/csvtojavaobject/Student.java
@@ -0,0 +1,17 @@
+package com.csvhandling.csvtojavaobject;
+
+
+
+class Student {
+    int id;
+    String name;
+    int age;
+    int marks;
+
+    Student(int id, String name, int age, int marks) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+}
